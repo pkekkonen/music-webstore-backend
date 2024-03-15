75d76c1923487dbed6b3482de2fef901a37e0637
pkekkonen <paulina.kekkonen@hotmail.com>
2024-03-15 11:30:58 +0100

    Initial setup for spring and db connection (db connection details stored as env var)

---
+++ src/main/java/com/booleanuk/musicwebstorebackend/MusicWebstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.booleanuk.musicwebstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicWebstoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicWebstoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booleanuk/musicwebstorebackend/MusicWebstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booleanuk.musicwebstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicWebstoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
