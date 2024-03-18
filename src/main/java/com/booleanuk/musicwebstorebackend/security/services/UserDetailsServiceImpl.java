efa6fa82ed7431b463ef16d3ef08eea86c8d15dd
pkekkonen <paulina.kekkonen@hotmail.com>
2024-03-18 14:25:46 +0100

    Added some initial functionality for security

---
+++ src/main/java/com/booleanuk/musicwebstorebackend/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,39 @@
+package com.booleanuk.musicwebstorebackend.security.services;
+
+import com.booleanuk.musicwebstorebackend.model.User;
+import com.booleanuk.musicwebstorebackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() ->
+                new UsernameNotFoundException("User not found with username " + username));
+        return UserDetailsImpl.build(user);
+    }
+
+    private String getCurrentUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new IllegalStateException("User not authenticated");
+        }
+        return authentication.getName();
+    }
+    public boolean isCurrentUser(String username) {
+        String currentUsername = getCurrentUsername();
+        return currentUsername.equals(username);
+    }
+
+}
