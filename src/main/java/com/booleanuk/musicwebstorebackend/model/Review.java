7e7235afce656a901f4f73b8150bf962ebed1e8d
Alexander <alexander.ellnestam@gmail.com>
2024-03-18 11:53:19 +0100

    Review model

---
+++ src/main/java/com/booleanuk/musicwebstorebackend/model/Review.java
@@ -0,0 +1,72 @@
+package com.booleanuk.musicwebstorebackend.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.catalina.User;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "reviews")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "content")
+    private String content;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    @Column
+    private OffsetDateTime createdAt;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    @Column
+    private OffsetDateTime updatedAt;
+
+   /* @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnoreProperties("review")
+    @JsonIncludeProperties(value = {"name", "email", "password"})
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    @JsonIgnoreProperties("review")
+    @JsonIncludeProperties(value = {"title", "release_year", "price"})
+    private Product product;
+
+    */
+
+    @PrePersist
+    public void prePersist() {
+        OffsetDateTime now = OffsetDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = OffsetDateTime.now();
+    }
+
+    public Review(int id) {
+        this.id = id;
+    }
+
+    public Review(String content) {
+        this.content = content;
+    }
+
+}
