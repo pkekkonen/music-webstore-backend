21e3e70499573ffd7e1e7c5e4d4cfd7bba968801
Alexander <alexander.ellnestam@gmail.com>
2024-03-18 10:29:17 +0100

    Genre model

---
+++ src/main/java/com/booleanuk/musicwebstorebackend/model/Genre.java
@@ -0,0 +1,61 @@
+package com.booleanuk.musicwebstorebackend.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "genres")
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    @Column
+    private OffsetDateTime createdAt;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    @Column
+    private OffsetDateTime updatedAt;
+
+//    @OneToMany(mappedBy = "genre")
+//    @JsonIgnoreProperties("genre")
+//    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+//    private List<ProductGenre> productGenres;
+
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        OffsetDateTime now = OffsetDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = OffsetDateTime.now();
+    }
+
+    public Genre(int id) {
+        this.id = id;
+    }
+
+
+}
+
